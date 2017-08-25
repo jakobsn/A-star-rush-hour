5c737b99f497ff9103862eddd2afe72d9bdcf9da
jakob <jakob_sn_@hotmail.com>
2017-08-25 13:58:50 +0200

    checkpoint before fix

--- astar_rush_hour.py
+++ astar_rush_hour.py
@@ -11,10 +11,12 @@
 import matplotlib.pyplot as plt
 from matplotlib import colors
 import numpy as np
+import copy
 
 def main():
     # TODO: Execute tasks
     board = Board("easy-3.txt")
+    board2 = copy.deepcopy(board)
     #board.create_board()
 
     print("Legal moves:")
@@ -27,12 +29,22 @@ def main():
         for vehicle in line:
             if(vehicle):
                 print("vehicle")
-                print(vehicle)
-                board.move_vehicle(vehicle, "forward")
+                print(vehicle.x, vehicle.y)
+                board.expand_move(vehicle, "forward")
                 break
         break
     """
+    for line in board.board:
+        print(line)
+    for line in board.board:
+        for vehicle in line:
+            if(vehicle):
+                board.expand_node()
+                break
+        break
+
     board.print_board()
+
 
 
 class ProblemSolver:
@@ -131,23 +143,49 @@ class Board:
         # LegalMoves format: [[$car, $direction][...]]
         legalMoves = []
         for vehicle in self.vehicles:
+            print("vehicle")
+            print(vehicle.x, vehicle.y)
             # Horizontal moves
             if(vehicle.orientation is 0):
+                print("Horizontal")
                 # Check forward move
-                if(not self.board[vehicle.x+1][vehicle.y] and vehicle.x+1 < self.width):
+                if(vehicle.x+vehicle.size < self.width and not self.board[vehicle.x+vehicle.size][vehicle.y]):
                     legalMoves.append([vehicle, "forward"])
                 # Check backward move
+                if(vehicle.x-1 > -1):
+                    print("x<-1")
+                print(self.board[vehicle.x-1][vehicle.y])
+                if(not self.board[vehicle.x-1][vehicle.y]):
+                    print("not empty")
                 elif(not self.board[vehicle.x-1][vehicle.y] and vehicle.x-1 > -1):
                     legalMoves.append([vehicle, "backward"])
             # Vertical moves
             else:
                 # Check forward move
-                if(not self.board[vehicle.x][vehicle.y+1] and vehicle.y+1 < self.height):
+                if(vehicle.y+vehicle.size < self.height and not self.board[vehicle.x][vehicle.y+vehicle.size]):
                     legalMoves.append([vehicle, "forward"])
                 # Check backward move
-                elif(not self.board[vehicle.x][vehicle.y+1] and vehicle.y-1 > -1):
+                elif(not self.board[vehicle.x][vehicle.y-1] and vehicle.y-1 > -1):
                     legalMoves.append([vehicle, "backward"])
         return legalMoves
+
+    # Get state from specific move (but dont do the move)
+    def expand_move(self, vehicle, direction):
+        board = copy.deepcopy(self)
+        board.move_vehicle(vehicle, direction)
+        return board
+
+    # Get all possible children (legal moves) after a move
+    def expand_node(self):
+        legalMoves = self.get_legal_moves()
+        children = []
+        print("Moves expanded:")
+        for move in legalMoves:
+            print(move[0].x, move[0].y, move[1])
+            children.append(self.expand_move(move[0],move[1]))
+        for child in children:
+            child.print_board()
+        return children
 
     # Print the board with help from matplotlib
     def print_board(self):
@@ -197,8 +235,13 @@ class Vehicle: #/?
 
 # The moves represents the nodes in the path
 class Move:
-    def __init__(self):
-        return
+    def __init__(self, vehicle, move, lastMove, g, h=0):
+        self.vehicle = vehicle
+        self.move = move
+        self.lastMove = lastMove
+        self.h = h
+        self.g = g + 1
+        self.f = g + h
 
 if __name__ == '__main__':
     main()
