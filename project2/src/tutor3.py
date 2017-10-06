749d8941abd24e19e675ecdef2095e832962239a
jakobsn <jakob_sn_@hotmail.com>
2017-10-06 12:19:07 +0200

    commit before bringing in new fileW

--- project2/src/tutor3.py
+++ project2/src/tutor3.py
@@ -368,17 +368,21 @@ def main(epochs=100, nbits=3, dims=[9, 3, 9], lrate=0.03, weight_range=None, sho
     ann.add_grabvar(1,'in') # Add a grabvar (to be displayed in its own matplotlib window).
     ann.add_grabvar(1,'out') # Add a grabvar (to be displayed in its own matplotlib window).
     ann.run(epochs)
-    #ann.runmore(epochs*2)
+    ann.runmore(epochs*2)
     return ann
 
+#datasets generic run
+#main(epochs=300,nbits=3,lrate=0.03,showint=100,mbs=9,vfrac=0.1,tfrac=0.1,vint=100,op_activation_funct=False, data_params=("../data/glass.txt",),
+#     dims=[9, 4, 4, 9], data_funct=readFile)
+
 #Translate default autoencoder to use generic main method
-main(epochs=300,nbits=4,lrate=0.03,showint=100,mbs=None,vfrac=0.1,tfrac=0.1,vint=100,op_activation_funct=False, data_params=(2**4,), dims=[16, 4, 16])
+#main(epochs=300,nbits=4,lrate=0.03,showint=100,mbs=None,vfrac=0.1,tfrac=0.1,vint=100,op_activation_funct=False, data_params=(2**4,), dims=[16, 4, 16])
 
 #parity
-#main(epochs=100, nbits=4, dims=[10, 2, 10+1], lrate=0.03, weight_range=None, showint=100, vint=100, data_params=(10, 10), data_funct=TFT.gen_vector_count_cases,
-#         steps=10, loss_funct=False, hl_activation_funct=False, op_activation_funct=True, case_fraction=1, vfrac=0.1, tfrac=0.1, mbs=10,
-#         map_batch_size=0, map_layers=0, map_dendrograms=[0], display_weights=[0], display_biases=[0])
 
+main(epochs=100, nbits=4, dims=[10, 2, 10+1], lrate=0.03, weight_range=None, showint=100, vint=100, data_params=(10, 10), data_funct=TFT.gen_vector_count_cases,
+         steps=10, loss_funct=False, hl_activation_funct=False, op_activation_funct=True, case_fraction=1, vfrac=0.1, tfrac=0.1, mbs=10,
+         map_batch_size=0, map_layers=0, map_dendrograms=[0], display_weights=[0], display_biases=[0])
 #Default autoencoder
 #autoex(epochs=300,nbits=4,lrate=0.03,showint=100,mbs=None,vfrac=0.1,tfrac=0.1,vint=100,sm=False)
 
