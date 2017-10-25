1f7ffcb8bc3509767a5703af0ebeb8225a3faacb
jakobsn <jakob_sn_@hotmail.com>
2017-10-25 21:22:20 +0200

    happy?

--- project2/src/tutor3.py
+++ project2/src/tutor3.py
@@ -450,7 +450,7 @@ class Caseman():
 def main(data_funct=readFile, data_params=("../data/glass.txt","avgdev"), epochs=1000, dims=[9, 9, 7], lrate=0.1, mbs=10, showint=0, vint=1000,
          vfrac=0.1, tfrac=0.1,hl_funct=tf.nn.sigmoid, ol_funct=tf.nn.softmax, loss_funct=crossEntropy, weight_range=[-.1, .1],
          cfrac=1, map_batch_size=0,map_layers=[], map_dendrograms=[], display_weights=[], display_biases=[], bestk=1,
-         onezero=False, komma=True, punktum=False,  decimals=2, mapping_time=30, dendro_time=30):
+         onezero=False, komma=True, punktum=False,  decimals=2, mapping_time=10, dendro_time=10):
     start = time()
     case_generator = (lambda : data_funct(*data_params))
     cman = Caseman(cfunc=case_generator,vfrac=vfrac,tfrac=tfrac,cfrac=cfrac)
