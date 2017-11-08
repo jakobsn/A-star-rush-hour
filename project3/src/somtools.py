de1ff6b3f17b14ddacd49f2b27de85d097c74237
jakobsn <jakob_sn_@hotmail.com>
2017-11-08 14:05:27 +0100

    scale avgdev

--- project3/src/somtools.py
+++ project3/src/somtools.py
@@ -1,9 +1,9 @@
 #!/usr/bin/py   thon
 
 import numpy as np
-from math import exp
+from math import exp, sqrt
 
-def readTSP(targetFile):
+def readTSP(targetFile, scale='avgdev'):
     with open(targetFile) as file:
         data = []
         lines = 0
@@ -18,7 +18,45 @@ def readTSP(targetFile):
         # Remove empty lines
         while not len(data[-1]):
             data.pop()
-    return scale_min_max(data, np.amin(data), np.amax(data))
+    if scale == 'avgdev':
+        return scale_min_max(data, np.amin(data), np.amax(data))
+    else:
+        return scale_min_max(data, np.amin(data), np.amax(data))
+
+# Scale data by average and standard deviation
+def scale_average_and_deviation(data):
+    features = get_data_features(data)
+    averages, deviations = calculate_average_and_standard_deviation(features)
+    for r, row in enumerate(data):
+        for f, feature in enumerate(row[0]):
+            data[r][0][f] = ((data[r][0][f]-averages[f])/deviations[f])
+    return data
+
+# Calculates average and standard deviation, followed wikipedia: https://simple.wikipedia.org/wiki/Standard_deviation
+def calculate_average_and_standard_deviation(features):
+    deviations = []
+    averages = []
+    for feature in features:
+        # Calculate average and standard deviation for each feature
+        averages.append(sum(feature)/len(feature))
+        squared_differences = []
+        for difference in feature:
+            squared_differences.append((difference-averages[-1])**2)
+        deviations.append(sqrt((sum(squared_differences)/len(squared_differences))))
+    return averages, deviations
+
+# Get all the input data stored in lists of features
+def get_data_features(data):
+    # Create list to store lists of input features
+    features = []
+    for x in range(len(data[0][0])):
+        features.append([])
+
+    # Append all features
+    for y in range(len(data)):
+        for x in range(len(data[y][0])):
+            features[x].append(data[y][0][x])
+    return features
 
 def euclidian_distance(x, y):
     #TODO
