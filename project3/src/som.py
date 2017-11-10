90442ef8dce8148888146311a949c62c1d32417a
jakobsn <jakob_sn_@hotmail.com>
2017-11-10 10:14:11 +0100

    added interface options for display time

--- project3/src/som.py
+++ project3/src/som.py
@@ -12,7 +12,8 @@ from random import randint
 
 class SOM:
     def __init__(self, epochs, lrate, hoodsize, insize, outsize, features, weight_range,
-                 lrate_decay, hood_decay, topo, lrConstant, hoodConstant):
+                 lrate_decay, hood_decay, topo, lrConstant, hoodConstant, showint,
+                 show_sleep):
         self.epochs = epochs
         self.features = features
         np.random.shuffle(self.features)
@@ -37,7 +38,10 @@ class SOM:
         self.neuronRing = self.create_neuron_ring()
         self.i = 0 # Keep track of which weight being changed for input neuron
         self.isDisplayed = False
+        self.showint = showint
+        self.show_sleep = show_sleep
         self.do_training()
+
 
 
     def do_training(self):
@@ -61,8 +65,8 @@ class SOM:
             self.hoodsize = round(self.hood_decay(epoch, self.initial_hood, self.hoodConstant, self.epochs))
             self.lrate = self.lrate_decay(epoch, self.initial_lrate,self.lrConstant, self.epochs)
             print("hood, lrate", self.hoodsize, self.lrate)
-            if epoch == 0 or epoch % 1000 == 0:
-                self.do_mapping(self.weight_range, self.hoodsize, self.lrate, epoch)
+            if epoch == 0 or epoch % self.showint == 0:
+                self.do_mapping(self.weight_range, self.hoodsize, self.lrate, epoch, self.show_sleep)
         return
 
     def findWinner(self, feature):
@@ -129,14 +133,14 @@ class SOM:
         return np.random.uniform(self.weight_range[0], self.weight_range[1], [self.insize, self.outsize])
 
 
-    def do_mapping(self, weight_range=None, hood=None, lrate=None, epochs=None, step='NA'):
+    def do_mapping(self, weight_range=None, hood=None, lrate=None, epochs=None, step='NA', sleep_time=1):
         if hood == None: hood=self.hoodsize
         if lrate == None: lrate=self.lrate
         if epochs == None: epochs=self.epochs
         if weight_range == None: weight_range=self.weight_range
         #print(self.features)
         if self.isDisplayed:
-            sleep(1)
+            sleep(sleep_time)
             PLT.close("all")
 
         fig = PLT.figure()
@@ -169,22 +173,24 @@ class SOM:
 
         PLT.show(block=False)
         self.isDisplayed = True
+
         #TODO
         return
 
 
-def main(data_funct=st.readTSP, data_params=('../data/6.txt','avgdev'), epochs=10000,  lrate=0.1, hoodsize=4,
-         insize=2, outsize=60, weight_range=[0.45,0.55], lrate_decay=st.powerDecay, hood_decay=st.exponentialDecay,
-         topo='ring', lrConstant=0.09, hoodConstant=200):
+def main(data_funct=st.readTSP, data_params=('../data/6.txt',None), epochs=10000,  lrate=0.2, hoodsize=4,
+         insize=2, outsize=60, weight_range=[1,5], lrate_decay=st.powerDecay, hood_decay=st.exponentialDecay,
+         topo='ring', lrConstant=0.09, hoodConstant=200, showint=2000, show_sleep=1, final_sleep=20):
     features =  data_funct(*data_params)
     som = SOM(epochs=epochs, lrate=lrate, hoodsize=hoodsize, features=features, insize=insize, outsize=outsize,
               weight_range=weight_range, lrate_decay=lrate_decay, hood_decay=hood_decay, topo=topo,
-              lrConstant=lrConstant, hoodConstant=hoodConstant)
+              lrConstant=lrConstant, hoodConstant=hoodConstant, showint=showint, show_sleep=show_sleep)
     print('funct', data_funct, 'params', data_params, 'epochs', epochs, 'lrate', lrate, '\n',
           'hoodsize', hoodsize, 'insize', insize, 'outsize', outsize,  'weight_range', weight_range, '\n',
           'lrate_deacay', lrate_decay, 'hood_decay', hood_decay,   '\n',
-          'topo', topo, "lrConstant", lrConstant, "hoodConstant", hoodConstant)
-    som.do_mapping(weight_range, hoodsize, lrate, epochs)
+          'topo', topo, "lrConstant", lrConstant, "hoodConstant", hoodConstant, '\n',
+          "showint", showint, 'show_sleep', show_sleep, 'final_sleep', final_sleep)
+    som.do_mapping(weight_range, hoodsize, lrate, epochs, 'Final', final_sleep)
 
 
 main()
